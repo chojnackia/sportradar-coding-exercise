f96ba3969a1f10e39f66f9c4b51a0d8305ad418d
chojnackia <56468528+chojnackia@users.noreply.github.com>
2023-06-18 14:24:36 +0200

    added business logic to FootballScoreboard methods

--- lib/src/main/java/sportradar/coding/exercise/scoreboard/impl/FootballScoreboard.java
+++ lib/src/main/java/sportradar/coding/exercise/scoreboard/impl/FootballScoreboard.java
@@ -1,30 +1,56 @@
 package sportradar.coding.exercise.scoreboard.impl;
 
 import sportradar.coding.exercise.match.Match;
+import sportradar.coding.exercise.match.impl.FootballMatch;
 import sportradar.coding.exercise.scoreboard.Scoreboard;
+import sportradar.coding.exercise.scoreboard.exception.MatchNotFoundException;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class FootballScoreboard implements Scoreboard {
     private List<Match> matches;
 
+    public FootballScoreboard() {
+        this.matches = new ArrayList<>();
+    }
+
     @Override
     public void startMatch(String homeTeam, String awayTeam) {
-
+        matches.add(new FootballMatch(homeTeam, awayTeam));
     }
 
     @Override
     public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
-
+        Match match = findMatch(homeTeam, awayTeam);
+        match.updateScore(homeScore, awayScore);
+        sortMatches();
     }
 
     @Override
     public void finishMatch(String homeTeam, String awayTeam) {
-
+        matches.remove(findMatch(homeTeam, awayTeam));
     }
 
     @Override
     public List<Match> getSummary() {
-        return null;
+        return matches;
+    }
+
+    private void sortMatches() {
+        matches.sort(Comparator.comparingInt(match -> match.getHomeScore() + match.getAwayScore()));
+        Collections.reverse(matches);
+    }
+
+    private Match findMatch(String homeTeam, String awayTeam) {
+        return matches.stream()
+                .filter(match ->
+                        match.getHomeTeam().equals(homeTeam)
+                                &&
+                                match.getAwayTeam().equals(awayTeam))
+                .findFirst()
+                .orElseThrow(MatchNotFoundException::new);
     }
 }
